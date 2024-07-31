77549e3074b33d5a58e417f323b49b8d66ddc68c
yeshu130 <yeswanthreddyp134@gmail.com>
2024-07-31 11:23:37 +0530

    Update nexusDownload.groovy

--- vars/nexusDownload.groovy
+++ vars/nexusDownload.groovy
@@ -1,5 +1,5 @@
 def call(artifactId,repoName,groupId,version){
     withCredentials([usernameColonPassword(credentialsId: 'nexus3', variable: 'nexusCreds')]) {
-        sh "curl -u ${nexusCreds} -o ${artifactId}.war 'http://172.31.11.195:8081/service/rest/v1/search/assets/download?repository=${repoName}&group=${groupId}&name=${artifactId}&version=${version}&ext=war'"
+        sh "curl -u ${nexusCreds} -o ${artifactId}.war 'http://172.31.29.0:8081/service/rest/v1/search/assets/download?repository=${repoName}&group=${groupId}&name=${artifactId}&version=${version}&ext=war'"
     }
 }
